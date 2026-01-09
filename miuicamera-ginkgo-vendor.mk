2da8848879b0ca6245ade438d3410fc427fbd470
neophyte <neophyteplayground@gmail.com>
2026-01-09 18:47:49 +0100

    miuicamera: init for ginkgo/willow

---
+++ miuicamera-ginkgo-vendor.mk
@@ -0,0 +1,31 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuicamera-ginkgo
+
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libCameraEffectJNI.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libCameraEffectJNI.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libarc_layer_sgl.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libarc_layer_sgl.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libarcsoft_wideselfie.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libarcsoft_wideselfie.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libcamera_arcsoft_beautyshot.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcamera_arcsoft_beautyshot.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libcamera_arcsoft_handgesture.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcamera_arcsoft_handgesture.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libcamera_beauty_mpbase.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcamera_beauty_mpbase.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libcamera_handgesture_mpbase.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcamera_handgesture_mpbase.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libcamera_wideselfie_mpbase.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcamera_wideselfie_mpbase.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libcom.xiaomi.camera.requestutil.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcom.xiaomi.camera.requestutil.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libhandengine.arcsoft.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libhandengine.arcsoft.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libjni_arcsoft_beautyshot.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libjni_arcsoft_beautyshot.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libjni_jpegutil_xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libjni_jpegutil_xiaomi.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libjni_wideselfie.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libjni_wideselfie.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libmorpho_memory_allocator.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libmorpho_memory_allocator.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libmorpho_panorama_gp3.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libmorpho_panorama_gp3.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/priv-app/MiuiCamera/lib/arm64/libmorpho_sensor_fusion.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libmorpho_sensor_fusion.so \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/vendor/camera/fonts/MFYueYuan-Regular.ttf:$(TARGET_COPY_OUT_VENDOR)/camera/fonts/MFYueYuan-Regular.ttf \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/vendor/camera/fonts/MI+LanTing_GB+Outside+YS_V2.3_20160322.ttf:$(TARGET_COPY_OUT_VENDOR)/camera/fonts/MI+LanTing_GB+Outside+YS_V2.3_20160322.ttf \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/vendor/camera/fonts/MIUI_Time.ttf:$(TARGET_COPY_OUT_VENDOR)/camera/fonts/MIUI_Time.ttf \
+    vendor/xiaomi/miuicamera-ginkgo/proprietary/vendor/etc/camera/beauty_ui9_intelligent_params.config:$(TARGET_COPY_OUT_VENDOR)/etc/camera/beauty_ui9_intelligent_params.config
+
+PRODUCT_PACKAGES += \
+    MiuiCamera
